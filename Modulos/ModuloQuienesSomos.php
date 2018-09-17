70ce21cb8e382ac1e520b688ab81b99886b0c25b
FMerchan <merchansalvatierra@gmail.com>
2018-09-17 19:43:05 -0300

    Se suben modificaciones.

---
+++ Modulos/ModuloQuienesSomos.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$html = "<link rel='stylesheet' type='text/css' href='Style/styleModQuienesSomos.css'>".
+		"<div class = 'body-mod-quienes-somos' align='center'>".
+		"<span class='body-mod-quienes-somos-text'>".
+		"Somos un equipo preparado orientado a dar soluciones de sistemas, ".
+		"tratamos de dar soluciones de calidad y resolver con rapides todos sus problemas.<br>".
+		"Somos un equipo preparado orientado a dar soluciones de sistemas, ".
+		"tratamos de dar soluciones de calidad y resolver con rapides todos sus problemas.<br>".
+		"</span>".
+		"</div>";
+
+
+?>
+
+
+
--- index.php
+++ index.php
@@ -20,6 +20,45 @@ $context = file_get_contents("indexTemplate.html");
 
 $configuracion = $config;
 
+// Armo el area de opciones centrales
+$menus = array( array( 'txtId' => "quienes_somos" , "title" => "Quienes Somos", "moduloName" => "ModuloQuienesSomos.php" ) ,
+				array( 'txtId' => "contactanos" , "title" => "Contactanos", "moduloName" => "ModuloContactanos.php" ) );
+
+$fitst = true;
+foreach ($menus as $value) {
+
+	$classActive = '';
+	if( $fitst === true )
+	{
+		$classActive = "active";
+		$fitst = false;
+	}
+	$options[] = "<li class = 'geekit-body-tab $classActive' >
+						<a class='geekit-body-tab-option' id='" . $value["txtId"] . "' src='Modulos/".$value["moduloName"]."' >".
+							"<span> ".
+								$value["title"].
+							"</span>".
+						"</a>".
+					"</li>";
+}
+
+require_once("Modulos/".$menus[0]['moduloName']);
+$body = "<div class='geekit-body-tab-container'>".	
+			"<ul class='geekit-body-tab-ul' >". 
+				implode("", $options ) . 
+			"</ul>".
+		"</div>".
+			"<div class = 'geekit-body-selected-tab'>".
+				$html .
+			"</div>";
+// Armo el area de la firma
+if( strpos($context, '[CONTAINERS]') !== false )
+{
+	$context = str_replace("[CONTAINERS]", $body, $context);
+}
+
+
+// Armo el area de la firma
 if( strpos($context, '[REDES]') !== false )
 {
 	$redes = buildSocialNetworks($configuracion);
@@ -46,6 +85,11 @@ function buildSocialNetworks( $config )
 	}
 
 	return implode("", $sites);
+}
+
+function getMenuOptions()
+{
+
 }
 
 ?>
